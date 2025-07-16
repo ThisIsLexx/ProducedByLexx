06885c1721c15d01da30c3fecaaea431a7540daf
Alexis Omar Guzmán Castillo <alexis.guzman8890@alumnos.udg.mx>
2025-07-16 02:25:13 -0600

    ui: created service component for landing page

---
+++ ProducedByLexx/src/components/Servicio.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    titulo: string;
+    descripcion: string;
+}>();
+</script>
+
+<template>
+    <div
+        class="bg-gray-700/30 p-6 h-[320px] max-h-full rounded-lg shadow-md hover:shadow-lg hover:scale-103 hover:cursor-pointer transition-all duration-150 ease-linear">
+        <h3 class="text-xl w-full flex justify-center font-semibold mb-2 text-[#00FFD1]">{{ props.titulo }}
+        </h3>
+        <p class="text-gray-700">{{ props.descripcion }}</p>
+    </div>
+</template>
